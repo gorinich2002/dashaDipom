04b86221450e8f5eb7701a81a66ccca1b8a08213
oleg <egormaysnikov@gmail.com>
2023-05-31 00:22:19 +0300

    change port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 // start express server on port 5000
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+app.listen(8080, () => {
+  console.log("server started on port 8080");
   }
 )
 app.use(express.static("build"));
