aee3d309762cfe1cafb7995c75e0665283367191
oleg <egormaysnikov@gmail.com>
2023-05-31 00:20:31 +0300

    add server

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express(); // create express app
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 // start express server on port 5000
 app.listen(3000, () => {
   console.log("server started on port 3000");
   }
 )
-app.use(express.static("public"));
+app.use(express.static("build"));
 
